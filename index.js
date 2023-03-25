01b38b88d939e7f34ada8b856e2ad36bb6ca98a9
Leroy G <liguan698@gmail.com>
2023-03-25 14:39:51 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+const streetNumOfHq = 42
+function distanceFromHqInBlocks(numOfStreet) {
+    return Math.abs(numOfStreet - streetNumOfHq)
+}
+
+function distanceFromHqInFeet(numOfStreet) {
+    return 264*distanceFromHqInBlocks(numOfStreet);
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(destination - start) * 264
+}
+
+function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(start,destination)
+    switch(true) {
+        case distance <= 400 : return 0;
+        case distance > 400 && distance <=2000 : return 0.02 * (distance-400);
+        case distance > 2000 && distance <=2500: return 25;
+        case distance > 2500: return 'cannot travel that far';
+    }
+}
